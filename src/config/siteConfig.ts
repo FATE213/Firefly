4c9017f8ee18d0d05f4e2f71bc84d25da3a90b3e
FATE213 <130949661+FATE213@users.noreply.github.com>
2026-07-26 18:53:43 +0800

    CHANGE

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,13 +6,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "FATESKY",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Hello!",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "http://fate213.uk/",
 
 	// 站点描述
 	description:
@@ -77,7 +77,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "FATESKY",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
